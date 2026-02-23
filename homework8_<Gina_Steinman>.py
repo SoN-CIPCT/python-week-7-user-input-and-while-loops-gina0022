b0d80debc3bb40f60bd3753316c89e0736b7cafe
gina0022 <gina.steinman@outlook.com>
2026-02-23 05:05:31 +0000

    Week 7 homework 8

--- homework8_<Gina_Steinman>.py
+++ homework8_<Gina_Steinman>.py
@@ -1,1 +1,10 @@
-
+pizza_orders=["cheese", "pepperoni", "canadian bacon", "Sausage", "black olive"]
+finished_pizzas []
+while pizza_orders:
+    current_pizza = pizza_orders.pop(0)
+print("Your pizza pie is finished!!")
+finished_pizzas.append(current_pizza)
+print("\nAll pizzas are finished:\n")
+for pizza in finished_pizzas:
+    print(f"The pizza {pizza} was made.")
+    
